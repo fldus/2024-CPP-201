2500d2099faf75c4ac6a1fbd447fef3f18fc1cdc
Riyeon Kim <s2319@e-mirim.hs.kr>
2024-03-18 15:05:47 +0900

    함수 오버로딩 적용

--- 2024-CPP-201/02-02 함수 오버로딩.cpp
+++ 2024-CPP-201/02-02 함수 오버로딩.cpp
@@ -1,15 +1,17 @@
 ﻿#include <iostream>
 using namespace std;
 
-int sum_int(int a, int b) {
+// 함수 오버로딩을 사용하면 함수이름을 짓는데 부담을 줄일 수 있다.
+
+int sum(int a, int b) {
 	return a + b;
 }
 
-float sum_float(float a, float b) {
+float sum(float a, float b) {
 	return a + b;
 }
 
 
 void main(void) {
-	cout << sum_int(1, 2) << sum_float(1.1f, 2.2f) << endl;
+	cout << sum(1, 2) << sum(1.1f, 2.2f) << endl;
 }
