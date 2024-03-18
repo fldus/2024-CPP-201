e39332d2efba385efe0ba2b8d7df685e101a4251
Riyeon Kim <s2319@e-mirim.hs.kr>
2024-03-18 15:02:46 +0900

    함수 오버로딩이 안될 때

---
+++ 2024-CPP-201/02-02 함수 오버로딩.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int sum_int(int a, int b) {
+	return a + b;
+}
+
+float sum_float(float a, float b) {
+	return a + b;
+}
+
+
+void main(void) {
+	cout << sum_int(1, 2) << sum_float(1.1f, 2.2f) << endl;
+}
