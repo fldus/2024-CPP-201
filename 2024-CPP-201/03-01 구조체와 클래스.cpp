759ebdb663abc7051a7ece89e4895ed6fce7bef9
fldus <kry59965@gmail.com>
2024-04-12 08:55:25 +0900

    생성자 오버로딩

--- 2024-CPP-201/03-01 구조체와 클래스.cpp
+++ 2024-CPP-201/03-01 구조체와 클래스.cpp
@@ -5,22 +5,23 @@ using namespace std;
 // 클래스는 디폴트가 private
 class Student {
 public:
-	// 생성자 - 객체를 생성할 때 호출되는 함수
-	// 특징 - (return) 반환형이 없다.
-	Student(int hakbun, string name){ 
+	Student(int hakbun, string name){	// 생성자
 		hakbun_ = hakbun;
 		name_ = name;
 	}
+	Student() {	// 생성자 오버로딩
+		hakbun_ = 2121;
+		name_ = "JWP";
+	}
+
 private:
-	int hakbun_;	// 멤버변수 구분하기 위해 _ 붙임.
+	int hakbun_;
 	string name_;
 };
 
 void main(void) {
-	// 매개변수가 없는 생성자 호출
-	// Student juyoung = Student();
-	
-	// 매개변수가 있는 생성자
+	// 매개변수가 있는 생성자 호출
 	Student juyoung = Student(2115, "윤주영");
+	Student jwp = Student();
 
 }
