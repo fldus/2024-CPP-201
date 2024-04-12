3e9cff1e2d5d8ef3081acfdac8df1cf0bb27f4b9
fldus <kry59965@gmail.com>
2024-04-12 10:12:52 +0900

    03-01 객체배열 동적할당

--- 2024-CPP-201/03-01 구조체와 클래스.cpp
+++ 2024-CPP-201/03-01 구조체와 클래스.cpp
@@ -5,10 +5,9 @@ using namespace std;
 // 클래스는 디폴트가 private
 class Student {
 public:
-	Student(int hakbun, string name)	// 생성자
+	Student(int hakbun, string name)
 		: hakbun_(hakbun), name_(name)
-	{	
-		cout << "학번이 등록되었습니다." << endl;
+	{
 	}
 	Student()	// 생성자 오버로딩
 		: hakbun_(2121), name_("JWP")
@@ -26,14 +25,15 @@ private:
 };
 
 void main(void) {
-	// 동적할당 : heqp 영역에 할당. 실행시간(run time)에 메모리 크기 결정
-	Student* juyoung = new Student(2115, "윤주영");
-	// 정적 할당 : stack 영역에 할당. 컴파일 시간에 메모리 크기 결정
-	Student jwp = Student();
+	Student* stu = new Student[3]{
+		{2115, "윤주영" },
+		{2121, "JWP"},
+		{2104, "위즈덤"}
+	};
 
-	juyoung->print();
-	jwp.print();
-
-	// 동적할당된 메모리 해제
-	delete juyoung;
+	for (int i = 0; i < 3; i++)
+		stu[i].print();
+	
+	// 동적할당 된 배열 삭제
+	delete []stu;
 }
