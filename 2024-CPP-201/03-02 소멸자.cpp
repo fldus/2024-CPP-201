906c6ce37d4c82fafd1cbbca45342ebe990fbf06
fldus <kry59965@gmail.com>
2024-04-19 08:54:03 +0900

    03-02 소멸자

---
+++ 2024-CPP-201/03-02 소멸자.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+
+class Student {
+public:
+	Student(int hakbun, char* name)
+		: hakbun_(hakbun), name_(name)
+	{
+		this->hakbun_ = hakbun;
+		// 동적할당
+		name_ = new char[18];	// 멤버변수 할당 -> 소멸자 사용해야함.
+		// 동적할당된 위치(name_)에 "윤주영"(name)을 복사한다.
+		strcpy(name_, name);
+	}
+
+	Student(){}
+
+	// 소멸자 - 객체가 사라질 때(메모리 공간이 헤제될 때) 호출되는 함수
+	~Student()	 
+	{
+		delete []name_;
+	}
+
+	void print() {
+		cout << hakbun_ << name_ << endl;
+	}
+
+private:
+	int hakbun_;
+	char* name_;
+};
+
+void main(void) {
+	Student* stu = new Student(2115, (char*)"윤주영");	// 객체 생성
+	stu->print();
+
+	delete stu;	// 객체만 삭제
+}
