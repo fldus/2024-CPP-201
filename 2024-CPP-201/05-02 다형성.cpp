03ef19cf245ab12408eb60c2d36d82ab49a58426
fldus <kry59965@gmail.com>
2024-08-16 10:21:10 +0900

    동적할당 복습

---
+++ 2024-CPP-201/05-02 다형성.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal {
+public:
+	void bark()
+	{
+
+	}
+
+	void sleep()
+	{
+
+	}
+
+	void eat()
+	{
+
+	}
+
+private:
+	string name;
+	unsigned int age;
+};
+
+void main(void)
+{
+	Animal* ani = new Animal();
+
+}
