770fcd4a36eb4c237b1cc58064e0299ffb1329ed
fldus <kry59965@gmail.com>
2024-08-19 14:07:38 +0900

    생성자 복습

--- 2024-CPP-201/05-02 다형성.cpp
+++ 2024-CPP-201/05-02 다형성.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 class Animal {
 public:
+	Animal(string name, unsigned int age)
+		: name_(name), age_(age)
+	{
+		cout << "이름 " << name_ << endl;
+		cout << "나이 " << age_ << endl;
+	}
 	void bark()
 	{
 		cout << "잘 짖는다" << endl;
@@ -20,13 +26,19 @@ public:
 	}
 
 private:
-	string name;
-	unsigned int age;
+	string name_;
+	unsigned int age_;
 };
 
+class Human : public Animal {
+public:
+
+private:
+	bool right_;
+};
 void main(void)
 {
-	Animal* ani = new Animal();
+	Animal* ani = new Animal("ani", 18);
 	ani->bark();
 	ani->sleep();
 	ani->eat();
