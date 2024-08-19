f507697f7050317556bff4615ff9d056b5c0b3b0
fldus <kry59965@gmail.com>
2024-08-19 13:47:16 +0900

    동적할당 복습2

--- 2024-CPP-201/05-02 다형성.cpp
+++ 2024-CPP-201/05-02 다형성.cpp
@@ -6,17 +6,17 @@ class Animal {
 public:
 	void bark()
 	{
-
+		cout << "잘 짖는다" << endl;
 	}
 
 	void sleep()
 	{
-
+		cout << "잘 잔다" << endl;
 	}
 
 	void eat()
 	{
-
+		cout << "잘 먹는다" << endl;
 	}
 
 private:
@@ -27,5 +27,9 @@ private:
 void main(void)
 {
 	Animal* ani = new Animal();
+	ani->bark();
+	ani->sleep();
+	ani->eat();
 
+	delete ani;
 }
