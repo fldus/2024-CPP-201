9a598fcb51d9bca378d74b3e38675aefc28195db
fldus <kry59965@gmail.com>
2024-09-02 15:06:00 +0900

    다형성 예제1

---
+++ 2024-CPP-201/다형성예제.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+T sum(T a, T b) { return a + b; }
+
+void main(void) {
+	cout << sum<int>(1, 2) << endl;
+	cout << sum<float>(1.f, 2.f) << endl;
+	cout << sum<string>("1", "2") << endl;
+}
