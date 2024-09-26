717e1e5e6d0c18fc4e2b263ffd5f4ffc154487be
fldus <kry59965@gmail.com>
2024-09-27 08:51:39 +0900

    09-01 enum 적용 전

---
+++ 2024-CPP-201/09-01 enum.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+void main(void)
+{
+	// 0:한식, 1:일식, 2:중식
+	int style = 0;
+
+	switch (style)
+	{
+	case 0:
+		cout << "한식" << endl;
+		break;
+	case1:
+		cout << "일식" << endl;
+		break;
+	case2:
+		cout << "중식" << endl;
+		break;
+	}
+}
