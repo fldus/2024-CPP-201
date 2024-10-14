045e98c80d8edc3da78cf394e640dea36f305396
fldus <kry59965@gmail.com>
2024-10-14 13:40:57 +0900

    09-03 auto 적용 전

---
+++ 2024-CPP-201/09-03 auto.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void main(void)
+{
+	int a = 40;
+	cout << a << endl;
+
+	vector<string> v = { "Love", "Lady", "Baby" };
+	string b = v[2];
+	cout << b << endl;
+}
